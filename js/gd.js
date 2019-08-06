bdb3e41ea403e7a8c4affbbb424f9792e47e1647
Alexandre Baker <alexandre.baker@gmail.com>
2019-08-06 10:09:47 -0400

    adding fireworks

---
+++ js/gd.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent('trigger', {
+  init: function() {
+    setInterval(() => { this.trigger(); }, 1500);
+    document.addEventListener('click', () => {
+      this.trigger(); 
+    });
+  },
+
+  trigger: function() {
+    this.el.emit('particleplayerstart', {
+      position: new THREE.Vector3(
+        (Math.random() - 0.5) * 7,
+        Math.random() * 2,
+        -5 - Math.random() * 2
+      ),
+      rotation: new THREE.Euler(Math.random() * 1 - .5, 0, 0)
+    });
+  }
+});
